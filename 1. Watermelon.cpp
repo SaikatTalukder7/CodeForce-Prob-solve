b094cc7ab5c251a44309483557d50f4f3e259e22
Saikat Talukder <saikat60mu@gmail.com>
2025-07-29 21:06:07 +0600

    Update and rename 2. Watermelon.cpp to 1. Watermelon.cpp

---
+++ 1. Watermelon.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int w;
+    cin >> w;
+
+    if (w % 2 == 0 && w > 2)
+        cout << "YES";
+    else
+        cout << "NO";
+
+    return 0;
+}
+
+/*
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int w;
+    cout << "Enter the weight of the watermelon: ";
+    cin >> w;
+
+    if (w % 2 == 0 && w > 2)
+    {
+        cout << "YES, the watermelon can be divided evenly." << endl;
+    }
+    else
+    {
+        cout << "NO, it can't be divided into two even positive parts." << endl;
+    }
+    return 0;
+}*/
