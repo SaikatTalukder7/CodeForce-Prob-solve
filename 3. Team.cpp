d008565e9782d7ef91d1f1cb951adcc7178eecb0
Saikat Talukder <saikat60mu@gmail.com>
2025-07-29 21:15:03 +0600

    Update 3. Team.cpp

--- 3. Team.cpp
+++ 3. Team.cpp
@@ -1,4 +1,27 @@
-#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n, p, v, t, count = 0;
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> p >> v >> t;
+        
+        if (p + v + t >= 2)
+        {
+            count++;
+        }
+    }
+
+    cout << count << endl;
+    return 0;
+}
+
+/*
+#include <bits/stdc++.h>
 using namespace std;
 
 int main()
@@ -20,3 +43,4 @@ int main()
     cout << "total number of problems they will solve: " << count << endl;
     return 0;
 }
+*/
