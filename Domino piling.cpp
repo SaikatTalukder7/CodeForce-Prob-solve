b8d2967e59c86865e3e004a817315e96d413d4e2
Saikat Talukder <saikat60mu@gmail.com>
2025-08-02 19:30:45 +0600

    Add files via upload

---
+++ Domino piling.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int m, n;
+    cin >> m >> n;
+    cout << (m * n) / 2 << endl;
+    return 0;
+}
+
+/*
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int m, n;
+
+    cout << "Enter the number of rows (m): ";
+    cin >> m;
+
+    cout << "Enter the number of columns (n): ";
+    cin >> n;
+
+    int maxDominoes = (m * n) / 2;
+
+    cout << "Maximum number of dominoes that can be placed: " << maxDominoes << endl;
+
+    return 0;
+}
+*/
