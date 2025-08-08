5526bcaff949574871b06e3c676f08ea89799349
Saikat Talukder <saikat60mu@gmail.com>
2025-08-08 19:51:27 +0600

    Add files via upload

---
+++ Bear and Big Brother.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+
+    int years = 0;
+    while (a <= b) {
+        a *= 3;
+        b *= 2;
+        years++;
+    }
+
+    cout << years << '\n';
+    return 0;
+}
+
+/*
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a, b;
+    int years = 0;
+
+    cout << "Enter Limak's weight and Bob's weight: ";
+    cin >> a >> b;
+
+    while (a <= b) {
+        a *= 3;
+        b *= 2;
+        years++;
+        cout << "After year " << years << ": Limak = " << a << ", Bob = " << b << endl;
+    }
+
+    cout << "Number of years until Limak becomes heavier: " << years << endl;
+
+    return 0;
+}
+*/
