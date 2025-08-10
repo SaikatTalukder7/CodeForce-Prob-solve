0335ec830b7cdfd9f390c1d3264ab4d4896d0dd9
Saikat Talukder <saikat60mu@gmail.com>
2025-08-10 18:22:29 +0600

    Add files via upload

---
+++ Elephant.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x;
+    cin >> x;
+    cout << (x + 4) / 5 << endl;
+    return 0;
+}
+
+/*
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x;
+    cout << "Enter friend's house position: ";
+    cin >> x;
+
+    int steps = (x + 4) / 5; // Same formula
+    cout << "Minimum steps: " << steps << endl;
+
+    return 0;
+}
+
+*/
