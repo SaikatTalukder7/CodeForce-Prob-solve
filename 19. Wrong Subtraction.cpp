c00fc876686d16337841b6df8eace8f77fad4fa0
Saikat Talukder <saikat60mu@gmail.com>
2025-08-14 02:00:35 +0600

    Update 19. Wrong Subtraction.cpp

--- 19. Wrong Subtraction.cpp
+++ 19. Wrong Subtraction.cpp
@@ -11,3 +11,22 @@ int main() {
     cout << n << "\n";
     return 0;
 }
+
+/*
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    long long n; int k;
+    cout << "Enter n and k: ";
+    cin >> n >> k;
+
+    for (int i = 0; i < k; ++i) {
+        if (n % 10 != 0) n--;
+        else n /= 10;
+    }
+
+    cout << "Result: " << n << endl;
+    return 0;
+}
+*/
