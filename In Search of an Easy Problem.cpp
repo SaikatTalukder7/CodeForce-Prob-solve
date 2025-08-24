fb183da3b494bb9e838d2e6d8cac5ebc1d495aeb
Saikat Talukder <saikat60mu@gmail.com>
2025-08-24 19:10:22 +0600

    Add files via upload

---
+++ In Search of an Easy Problem.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int x;
+    bool hard = false;
+    for (int i = 0; i < n; i++) {
+        cin >> x;
+        if (x == 1) {
+            hard = true;
+        }
+    }
+    if (hard) cout << "HARD\n";
+    else cout << "EASY\n";
+    return 0;
+}
+
+
+/*
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    bool hard = false;
+    for (int i = 0; i < n; i++) {
+        int x;
+        cin >> x;
+        if (x == 1) {
+            hard = true;
+        }
+    }
+    if (hard) cout << "HARD";
+    else cout << "EASY";
+    return 0;
+}
+*/
